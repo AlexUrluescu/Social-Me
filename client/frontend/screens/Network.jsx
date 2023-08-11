e161494aba9370c4b449adc052ad04a3aaf50a38
AlexUrluescu <alexurluescu23@gmail.com>
2023-08-11 21:10:14 +0300

    create the screens

---
+++ client/frontend/screens/Network.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+
+const Network = () => {
+  return (
+    <View>
+        <Text>Network</Text>
+    </View>
+  )
+}
+
+export default Network
