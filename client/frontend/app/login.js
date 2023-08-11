d8cf7e5a19619edbcfe0b76cff4fa8d41034e1ec
AlexUrluescu <alexurluescu23@gmail.com>
2023-08-11 21:39:18 +0300

    Create a navbar component

--- client/frontend/app/login.js
+++ client/frontend/app/login.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { StyleSheet, Text, View } from "react-native";
 
+import NavBar from '../components/NavBar';
+
 const Login = () => {
   return (
     <View>
+        <NavBar />
         <Text>Login</Text>
     </View>
   )
--- client/frontend/app/network.js
+++ client/frontend/app/network.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { StyleSheet, Text, View } from "react-native";
 
+import NavBar from '../components/NavBar';
+
 const Network = () => {
   return (
     <View>
+        <NavBar />
         <Text>Network</Text>
     </View>
   )
