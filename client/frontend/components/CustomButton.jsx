10564a5fb0e11929ebae5e1b13ee3a349d44ed09
AlexUrluescu <alexurluescu23@gmail.com>
2023-08-15 15:59:31 +0300

    Create a custom button

---
+++ client/frontend/components/CustomButton.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Text, StyleSheet, Pressable } from 'react-native';
+
+export default function Button({title, onPress}) {
+//   const { onPress, title = 'Save' } = props;
+  return (
+    <Pressable style={styles.button} onPress={onPress}>
+      <Text style={styles.text}>{title}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: "25px",
+    elevation: 3,
+    backgroundColor: 'black',
+    width: 60,
+    height: 60
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+});
